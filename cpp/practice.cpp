d73fefdbd28474ec60589d404a8726bc93bb9ab6
Asib27 <62117473+Asib27@users.noreply.github.com>
2022-11-26 01:21:41 +0600

    Update practice.cpp

--- cpp/practice.cpp
+++ cpp/practice.cpp
@@ -35,6 +35,66 @@ public:
     }   
 };
 
+static unsigned int SDBMHash(string str) {
+	unsigned int hash = 0;
+	unsigned int i = 0;
+	unsigned int len = str.length();
+
+	for (i = 0; i < len; i++)
+	{
+		hash = (str[i]) + (hash << 6) + (hash << 16) - hash;
+	}
+
+	return hash;
+}
+
+class SymbolInfo
+{
+	string _name;
+	string _type;
+	SymbolInfo* _next;
+public:
+	SymbolInfo() : _next(nullptr) {};
+	SymbolInfo(string name, string type, string next=nullptr)
+		: _name(name), _type(type), _next(next)
+	{
+
+	}
+
+	string getName(){ return _name;}
+	string getType(){ return _type;}
+	SymbolInfo* getNext(){ return _next;}
+	void setName(string name){ _name = name;}
+	void setType(string type){ _type = type;} 
+	void setNext(SymbolInfo* next){
+		_next = next;
+	}
+}
+
+/**
+* If any instance of ScopeTable is destroyed it will automatically free,
+* all symbolInfo it contaims
+*/
+class ScopeTable
+{
+	SymbolInfo* _table;
+	int _no_of_bucket;
+
+	
+public:
+	ScopeTable(int n) : _no_of_bucket(n){
+		table = new SymbolInfo[n];
+		for(int i = 0; i < n; i++) table[i] = nullptr;
+	}
+
+	~ScopeTable(){
+		for(int i = 0; i < _no_of_bucket; i++){
+			if(table[i]) delete table[i];
+		}
+		delete[] table;
+	}
+}
+
 int main()
 {
     cout << "Hello world" << endl;
